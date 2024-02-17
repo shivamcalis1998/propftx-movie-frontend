266646df94004ae13073483ddb2e2cb95d853494
Shivam Swami <114653221+shivamcalis1998@users.noreply.github.com>
2024-02-17 13:54:55 +0530

    Update MoviesGrid.jsx

--- src/components/MoviesGrid/MoviesGrid.jsx
+++ src/components/MoviesGrid/MoviesGrid.jsx
@@ -58,7 +58,7 @@ const MoviesGrid = ({ movies, handleDelete, HandleEditMovie, language }) => {
             marginLeft: "90%",
           }}
         >
-          No Movie avilable in {language} language....
+          No Movie available in {language} language....
         </h1>
       )}
     </div>
